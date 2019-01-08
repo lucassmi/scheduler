c555ccb61f3f367c32d6e9aaf2221ffb941ac686
lucas.soumaille <lucassmi33@hotmail.fr>
2019-01-08 11:24:03 +0100

    update task scheduler

--- main.py
+++ main.py
@@ -2,11 +2,11 @@ from task import Task
 from scheduler_task import SchedulerTask
 
 if __name__ == '__main__':
-    task_a = Task(1,20,"mabite")
-    task_b = Task(2,10,"mabite2")
-    task_c = Task(3,30,"mabite3")
-    task_d = Task(5,15,"mabite5")
-    task_e = Task(4,30,"mabite4")
+    task_a = Task(1,20,"job1")
+    task_b = Task(2,10,"job2")
+    task_c = Task(3,30,"job3")
+    task_d = Task(5,15,"job5")
+    task_e = Task(4,30,"job4")
 
     scheduler = SchedulerTask([task_a, task_b, task_c, task_d, task_e])
     scheduler.order_task()
